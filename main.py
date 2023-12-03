5b0d85237d195d5c9604a6b1555a8d1516cc09c7
Mikhail Karyagin <notan02091990@gmail.com>
2023-12-03 16:48:29 +0500

    TELEGRAMBOT

---
+++ main.py
@@ -0,0 +1,24 @@
+import logging
+from telegram import Update
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Привет!Давай начнем разговор!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('TOKEN').build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+  
+    
+    application.run_polling()
+
+
+    
